2baaaf9c9125eef48c743f47ac8d803575769473
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-06-02 01:22:25 -0500

    feat: Add 'Calculator' component
    * Add the styles for the component
    * Add the component in the App.js file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Import components 
+import Calculator from './components/Calculator/Calculator';
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +16,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Calculator />
       </div>
     );
   }
